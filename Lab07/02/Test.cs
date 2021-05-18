3390d586d88917d46cb7bc0f6c3ebcd90d022d9c
alexamikaya <alexamikaya@gmail.com>
2021-05-18 18:30:53 +0300

    1

---
+++ Lab07/02/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utils
+{
+    class Test
+    {
+        static void Main()
+
+        {
+
+            string message;
+            Console.WriteLine("Enter string to reverse:");
+            message = Console.ReadLine();
+            Utils.Reverse(ref message);
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
