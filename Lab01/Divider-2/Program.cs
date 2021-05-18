d4e10a0f4817126ae73e598ca3f2c49ef51b7cd4
alexamikaya <alexamikaya@gmail.com>
2021-05-18 13:49:15 +0300

    1

---
+++ Lab01/Divider-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Divider
+{
+    class DivideIt
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите первое число");
+                string temp = Console.ReadLine();
+                int i = Int32.Parse(temp);
+
+                Console.WriteLine("Введите второе число");
+                temp = Console.ReadLine();
+                int j = Int32.Parse(temp);
+                int k = i / j;
+                Console.WriteLine(k);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception was thrown: {0}", e);
+            }
+            Console.Read();
+        }
+    }
+}
