233cf611c601d52ff779031f418439268b38d442
alexamikaya <alexamikaya@gmail.com>
2021-05-18 13:39:45 +0300

    Add project files.

---
+++ Lab05/FileDetails-01/FileDetails.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileDetails_01
+{
+    class FileDetails
+    {
+        static void Main(string[] args)
+        {
+            string fileName = args[0];
+            FileStream stream = new FileStream(fileName, FileMode.Open);
+            StreamReader reader = new StreamReader(stream);
+            int size = (int)stream.Length;
+            char[] contents = new char[size];
+            for (int i = 0; i<size; i++)
+            {
+                contents[i] = (char)reader.Read();
+            }
+            foreach (char ch in contents)
+            {
+                Console.Write(ch);
+            }
+
+        }
+        static void Summarize(char[] contents)
+        {
+            int vowels = 0;
+            int consonants = 0;
+            int lines = 0;
+            foreach (char symbol in contents)
+            {
+                if (Char.IsLetter(symbol))
+                {
+                    if ("AEIOUaeiou".IndexOf(symbol) != -1)
+                    {
+                        vowels++;
+                    } else
+                    {
+                        consonants++;
+                    }
+                } else if (symbol == '\n')
+                {
+                    lines++;
+                }
+            }
+            Console.WriteLine("Total no of characters: {0}", contents.Length);
+
+            Console.WriteLine("Total no of vowels : {0}", vowels);
+
+            Console.WriteLine("Total no of consonants: {0}", consonants);
+
+            Console.WriteLine("Total no of lines : {0}", lines);
+
+        }
+    }
+}
---
+++ Lab05/MatrixMultiply-2/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixMultiply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[2, 2];
+            ///  a[0, 0] = 1; a[0, 1] = 2;
+            ///  a[1, 0] = 3; a[1, 1] = 4;
+            ///string s = Console.ReadLine( );
+            ///a[0, 0] = int.Parse(s);
+            ///string s1 = Console.ReadLine();
+            /// a[0, 1] = int.Parse(s1);
+            /// string s2 = Console.ReadLine();
+            /// a[1, 0] = int.Parse(s2);
+            /// string s3 = Console.ReadLine();
+            /// a[1, 1] = int.Parse(s3);
+            Input (a);
+            int[,] b = new int[2, 2];
+            Input(b);
+            ///b[0, 0] = 5; b[0, 1] = 6;
+            ///b[1, 0] = 7; b[1, 1] = 8;
+            ///int[,] result = new int[2, 2];
+            ///result[0, 0] = a[0, 0] * b[0, 0] + a[0, 1] * b[1, 0];
+            ///result[0, 1] = a[0, 0] * b[0, 1] + a[0, 1] * b[1, 1];
+            ///result[1, 0] = a[1, 0] * b[0, 0] + a[1, 1] * b[1, 0];
+            ///result[1, 1] = a[1, 0] * b[0, 1] + a[1, 1] * b[1, 1];
+            int [,] result = Multiply(a, b);
+            Output(result);
+        }
+        static void Input (int[,] dst)
+        {
+            for (int r = 0; r < 2; r++)
+            {
+
+                for (int c = 0; c < 2; c++)
+                {
+
+                    Console.Write("Enter value for [{0},{1}] : ", r, c);
+
+                    string s = Console.ReadLine();
+
+                    dst[r, c] = int.Parse(s);
+
+                }
+
+            }
+
+            Console.WriteLine();
+            ///string s = Console.ReadLine();
+            /// a[0, 0] = int.Parse(s);
+            ///string s1 = Console.ReadLine();
+            ///a[0, 1] = int.Parse(s1);
+            ///string s2 = Console.ReadLine();
+            ///a[1, 0] = int.Parse(s2);
+            ///string s3 = Console.ReadLine();
+            ///a[1, 1] = int.Parse(s3);
+        }
+        static int[,] Multiply(int[,] a, int[,] b)
+        {
+           int[,] result = new int[2, 2];
+            for (int r = 0; r < 2; r++)
+            {
+
+                for (int c = 0; c < 2; c++)
+                {
+
+                    result[r, c] += a[r, 0] * b[0, c] + a[r, 1] * b[1, c];
+
+                }
+
+            }
+            /// result[0, 0] = a[0, 0] * b[0, 0] + a[0, 1] * b[1, 0];
+            ///  result[0, 1] = a[0, 0] * b[0, 1] + a[0, 1] * b[1, 1];
+            /// result[1, 0] = a[1, 0] * b[0, 0] + a[1, 1] * b[1, 0];
+            /// result[1, 1] = a[1, 0] * b[0, 1] + a[1, 1] * b[1, 1];
+            return result;
+        }
+        static void Output(int[,] result)
+        {
+            /// Console.WriteLine(result[0, 0]);
+            /// Console.WriteLine(result[0, 1]);
+            /// Console.WriteLine(result[1, 0]);
+            ///  Console.WriteLine(result[1, 1]);
+            for (int r = 0; r < result.GetLength(0); r++)
+            {
+                for (int c = 0; c < result.GetLength(1); c++)
+                {
+                    Console.Write("{0} ", result[r, c]);
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+       
+    }
+}
